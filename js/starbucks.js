300fa5ca905ff08c18e2d9a012cd08ee51c5f538
gun1kim <obkh31@naver.com>
2023-09-25 09:51:58 +0900

    230922 js

---
+++ js/starbucks.js
@@ -0,0 +1,15 @@
+window.addEventListener('scroll', function() {
+    let favTexts = document.querySelectorAll(".fav-text");
+    let value = scrollY;
+    console.log(value);
+
+    if (value > 400) {
+        favTexts.forEach((item) => {
+            item.style.animation = "slide-backward 2s ease-in-out forwards";
+        })
+    } else {
+        favTexts.forEach((item) => {
+            item.style.animation = "slide-forward 2s ease-in-out";
+        })
+    }
+})
